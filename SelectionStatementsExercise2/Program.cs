ab74a46d925b6b8c0bea61471409fe791491650e
Autif Kamal <kautif@gmail.com>
2023-03-29 14:51:42 -0500

    added prompt and switch cases for school subjects

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What was your favorite subject in school? Use a capital letter for the start of each new word");
+            var subject = Console.ReadLine();
+
+            switch (subject) {
+                case "English":
+                    Console.WriteLine("Nice. I'm pretty good at writing essays");
+                    break;
+                case "Art":
+                    Console.WriteLine("Awesome. I'm actually learning how to draw right now but I was never good at it");
+                    break;
+                case "Math":
+                    Console.WriteLine("I'm good with Math until Algebra. After that, I probably need a calculator");
+                    break;
+                case "Social Studies":
+                    Console.WriteLine("That's cool. I think history on its own is interesting, but I just hate memorizing years, dates, and events");
+                    break;
+                case "Science":
+                    Console.WriteLine("Nice. I think the funnest part of science was doing the experiments");
+                    break;
+                default:
+                    Console.WriteLine("I don't think I took that subject or I forget which subjects I took");
+                    break;
+            }
         }
     }
 }
